83fdfa90820845079b352abe6e0d8f2b18971306
MrGrin91 <gorodnihi@yandex.ru>
2025-04-22 18:35:52 +0300

    Update Main.java

--- src/main/java/guru/qa/homework/VR-3/Main.java
+++ src/main/java/guru/qa/homework/VR-3/Main.java
@@ -2,6 +2,6 @@ package guru.qa.homework.VR;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello, from Vadim!");
+        System.out.printf("Hello, from Vadim!!!!!!!!!!!!!!!!!!");
         }
     }
